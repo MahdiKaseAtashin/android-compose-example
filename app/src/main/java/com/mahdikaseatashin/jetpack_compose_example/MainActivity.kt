2055836fedc5a37a03813ed65fc090dfdc72d9e4
Mahdi Kase Atashin <mahdikaseatashin@gmail.com>
2021-08-22 15:09:22 +0430

    + ui design list conversation

--- app/src/main/java/com/mahdikaseatashin/jetpack_compose_example/MainActivity.kt
+++ app/src/main/java/com/mahdikaseatashin/jetpack_compose_example/MainActivity.kt
@@ -3,36 +3,84 @@ package com.mahdikaseatashin.jetpack_compose_example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.mahdikaseatashin.jetpack_compose_example.ui.theme.JetpackcomposeexampleTheme
+import androidx.compose.ui.unit.dp
+import com.mahdikaseatashin.jetpack_compose_example.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackcomposeexampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            ComposeTutorialTheme {
+                MessageCard(Message("Android", "Jetpack Compose"))
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = null,
+                modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackcomposeexampleTheme {
-        Greeting("Android")
+        Column {
+            Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2
+                )
+            }
+        }
     }
 }
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+
+data class Message(val author: String, val body: String)
--- app/src/main/java/com/mahdikaseatashin/jetpack_compose_example/ui/theme/Theme.kt
+++ app/src/main/java/com/mahdikaseatashin/jetpack_compose_example/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun JetpackcomposeexampleTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun ComposeTutorialTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
